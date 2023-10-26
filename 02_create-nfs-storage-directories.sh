0e1bc74df4c46ecc9cda4da46010daa9d80c3218
Gerard Mortel <gmortel@us.ibm.com>
2023-10-26 16:29:38 -0500

    Moved data dircectory to user home

--- 02_create-nfs-storage-directories.sh
+++ 02_create-nfs-storage-directories.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # # Create the directory for the operator
-mkdir -p /data/nfsshare/operator
+mkdir -p ${HOME}/data/nfsshare/operator
 
 # Create the directory for the logs
-mkdir -p /data/nfsshare/logs
+mkdir -p ${HOME}/data/nfsshare/logs
 
 #Allow full access to both directories
-chmod -R 777 /data/nfsshare
+chmod -R 777 ${HOME}/data/nfsshare
 
 # Add new entries for operator and log to /etc/export file
-for ip in $(oc get nodes -o wide | grep -Ev INTERNAL-IP | awk '{ print $6 }'); do echo "/data/nfsshare        $ip(rw,sync,insecure,no_wdelay,no_root_squash)" >> /etc/exports; echo "/data/nfsshare/operator        $ip(rw,sync,insecure,no_wdelay,no_root_squash)" >> /etc/exports ; echo "/data/nfsshare/logs        $ip(rw,sync,insecure,no_wdelay,no_root_squash)" >> /etc/exports ; done
+for ip in $(oc get nodes -o wide | grep -Ev INTERNAL-IP | awk '{ print $6 }'); do echo "${HOME}/data/nfsshare        $ip(rw,sync,insecure,no_wdelay,no_root_squash)" >> /etc/exports; echo "${HOME}/data/nfsshare/operator        $ip(rw,sync,insecure,no_wdelay,no_root_squash)" >> /etc/exports ; echo "${HOME}/data/nfsshare/logs        $ip(rw,sync,insecure,no_wdelay,no_root_squash)" >> /etc/exports ; done
 
 # Make exportfs entries in /etc/export file permanent
 exportfs -rav
